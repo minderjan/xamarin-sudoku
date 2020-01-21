e5e1fc1d26ab50b6ecd2ecaa7da8b02b9bb50942
Jan Minder <minderjan@gmail.com>
2020-01-21 01:44:16 +0100

    add some basic grid service tests

---
+++ sudoku.Test/GridServiceTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using sudoku.Models;
+
+namespace sudoku.Test
+{
+    public class Tests
+    {
+
+        IGridService _gridServcie;
+        int totalFieldCount = 81;
+        int totalEmptyFieldsCount = 41;
+
+        [SetUp]
+        public void Setup()
+        {
+            _gridServcie = new GridService();
+
+        }
+
+        [Test]
+        public void TestGenerateInitialGrid()
+        {
+            Assert.That(_gridServcie.GenerateInitialGrid(), Has.Count.EqualTo(totalFieldCount));
+        }
+
+        [Test]
+        public void TestEmptyFieldsCount()
+        {
+            List<Field> sudokuGrid = _gridServcie.GenerateInitialGrid();
+            Assert.That(_gridServcie.GetEmptyFieldsCount(sudokuGrid), Is.EqualTo(totalEmptyFieldsCount));
+        }
+    }
+}
